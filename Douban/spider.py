23afe83d1e0ac9e2e4785c666af8b5b9defe2c3d
Riddle-S <1035572554@qq.com>
2020-11-16 20:54:05 +0800

    basic data 250

--- Douban/spider.py
+++ Douban/spider.py
@@ -1,30 +1,84 @@
-from bs4 import BeautifulSoup  # 网页解析，获取数据
-import re  # 正则表达式，文字匹配
-import urllib.request  # 制定URL,获取网页数据
-import urllib.error
-import xlwt  # excel
-import pymysql  # mysql
+import requests
+from bs4 import BeautifulSoup
+import xlwt
+import time
 
 
-def main():
-    baseurl = "https://movie.douban.com/top250?start="
-    # 1.爬取网页
-    datalist = getData(baseurl)
-    savepath = ".\\DoubanTop250.xls"
-    # 3.保存数据
-    saveData(savepath)
+class Douban:
+    def __init__(self):
+        self.URL = 'https://movie.douban.com/top250'
+        self.startnum = []
+        for start_num in range(0, 251, 25):
+            self.startnum.append(start_num)
+        self.header = {
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 Edg/86.0.622.69'
+        }
+
+    def get_top250(self):
+        datalist = []
+        for start in self.startnum:
+            start = str(start)
+            html = requests.get(self.URL,
+                                params={'start': start},
+                                headers=self.header)
+            soup = BeautifulSoup(html.text, 'html.parser')
+            # 1.所有电影信息存储在ol标签
+            Obj = soup.find('div', class_='article')
+
+            if Obj != None:
+                details = Obj.find_all('li')
+                for detail in details:
+                    data = []
+                    #排名
+                    data.append(detail.find('em').get_text())
+                    # 电影名称
+                    data.append(detail.find('span', class_='title').get_text())
+                    #图片链接
+                    picture = detail.find('img')
+                    data.append(picture.get('src'))
+                    # 电影年份
+                    #抓取年份、国家
+                    actor_infos = detail.find('p').get_text().strip().split(
+                        '\n')
+                    actor_infos1 = actor_infos[0].split('\xa0\xa0\xa0')
+                    director = actor_infos1[0]
+                    data.append(director)
+                    if len(actor_infos1) >= 2:
+                        role = actor_infos1[1]
+                    else:
+                        role = ''
+                    data.append(role)
+                    year_area = actor_infos[1].lstrip().split('\xa0/\xa0')
+                    data.append(year_area[0])
+                    data.append(year_area[1])
+                    data.append(year_area[2])
+
+                    data.append(0)
+                    data.append(0)
+                    data.append('')
+                    datalist.append(data)
+                    print("爬取完", data[0])
+                time.sleep(1)
+
+        self.saveData(datalist)
+
+    def saveData(self, datalist):
+        workbook = xlwt.Workbook(encoding='utf-8')
+        worksheet = workbook.add_sheet('Doubantop250', cell_overwrite_ok=True)
+        col = ('rank', 'name', 'picturelink', 'director', 'role', ' year',
+               'country', 'movietype', 'average_star', 'comment_num',
+               'content_tag')
+        for i in range(0, 11):
+            worksheet.write(0, i, col[i])
+        for i in range(250):
+            data = datalist[i]
+            for j in range(0, 11):
+                worksheet.write(i + 1, j, data[j])
+                print("写入", i + 1, "条")
+        workbook.save('douban.xls')
 
 
-def getData(baseurl):
-    '''爬取网页'''
-    datalist = []
-    # 2.逐一解析数据
-    return datalist
-
-
-def saveData(savepath):
-    pass
-
-
-if __name__ == "__main__":  # 执行当前程序
-    main()
+if __name__ == "__main__":
+    cls = Douban()
+    cls.get_top250()
--- Douban/test.py
+++ Douban/test.py
@@ -1,3 +1,17 @@
-import urllib.request
-response = urllib.request.urlopen(("http://www.baidu.com"))
-print(response.read())
+import xlrd
+import xlwt
+from xlutils.copy import copy
+# 打开想要更改的excel文件
+old_excel = xlrd.open_workbook('douban.xls', formatting_info=True)
+# 将操作文件对象拷贝，变成可写的workbook对象
+old_excel = copy(old_excel)
+# 获得第一个sheet的对象
+worksheet = old_excel.get_sheet(0)
+# 写入数据
+col = ('rank', 'name', 'picturelink', 'country', ' year', 'type', 'director',
+       'role', 'average_star', 'comment_num', 'content_tag')
+for i in range(0, 11):
+    worksheet.write(0, i, col[i])
+
+# 另存为excel文件，并将文件命名
+old_excel.save('douban.xls')
