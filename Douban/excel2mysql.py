427ced424f019c57b30111d8faa1235150a3866e
Riddle-S <1035572554@qq.com>
2020-11-17 21:09:16 +0800

    完善了部分数据

--- Douban/excel2mysql.py
+++ Douban/excel2mysql.py
@@ -1,8 +1,8 @@
 import xlrd
 import pymysql
 # Open the workbook and define the worksheet
-book = xlrd.open_workbook("douban.xlsx")
-sheet = book.sheet_by_name("douban")
+book = xlrd.open_workbook("movies.xlsx")
+sheet = book.sheet_by_name("movies")
 
 #建立一个MySQL连接
 database = pymysql.connect(host="localhost",
@@ -14,25 +14,47 @@ database = pymysql.connect(host="localhost",
 cursor = database.cursor()
 
 # 创建插入SQL语句
-query = """INSERT INTO introduction(rank, moviename, picturelink, director, movierole, movieyear,country, movietype, average_star, comment_num, content_tag) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-
+query = """Insert into movies(rank, moviename, picturelink,countryordistrict,movieyear,movietime,movietype1,movietype2,movietype3,movietype4,movietype5,director1,director2, movierole1, movierole2,movierole3,movierole4,
+average_star, comment_num, content_tag1,content_tag2,content_tag3,content_tag4,content_tag5,
+content_tag6,content_tag7,content_tag8) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s)"""
+#列名中不能带有/
 # 创建一个for循环迭代读取xls文件每行数据的, 从第二行开始是要跳过标题
 for r in range(1, sheet.nrows):
+
     rank = sheet.cell(r, 0).value
     moviename = sheet.cell(r, 1).value
     picturelink = sheet.cell(r, 2).value
-    director = sheet.cell(r, 3).value
-    movierole = sheet.cell(r, 4).value
-    movieyear = sheet.cell(r, 5).value
-    country = sheet.cell(r, 6).value
-    movietype = sheet.cell(r, 7).value
-    average_star = sheet.cell(r, 8).value
-    comment_num = sheet.cell(r, 9).value
-    content_tag = sheet.cell(r, 10).value
+    country = sheet.cell(r, 3).value
+    movieyear = sheet.cell(r, 4).value
 
-    values = (rank, moviename, picturelink, director, movierole, movieyear,
-              country, movietype, average_star, comment_num, content_tag)
-
+    movietime = sheet.cell(r, 5).value
+    movietype1 = sheet.cell(r, 6).value
+    movietype2 = sheet.cell(r, 7).value
+    movietype3 = sheet.cell(r, 8).value
+    movietype4 = sheet.cell(r, 9).value
+    movietype5 = sheet.cell(r, 10).value
+    director1 = sheet.cell(r, 11).value
+    director2 = sheet.cell(r, 12).value
+    movierole1 = sheet.cell(r, 13).value
+    movierole2 = sheet.cell(r, 14).value
+    movierole3 = sheet.cell(r, 15).value
+    movierole4 = sheet.cell(r, 16).value
+    average_star = sheet.cell(r, 17).value
+    comment_num = sheet.cell(r, 18).value
+    content_tag1 = sheet.cell(r, 19).value
+    content_tag2 = sheet.cell(r, 20).value
+    content_tag3 = sheet.cell(r, 21).value
+    content_tag4 = sheet.cell(r, 22).value
+    content_tag5 = sheet.cell(r, 23).value
+    content_tag6 = sheet.cell(r, 24).value
+    content_tag7 = sheet.cell(r, 25).value
+    content_tag8 = sheet.cell(r, 26).value
+    values = (rank, moviename, picturelink, country, movieyear, movietime,
+              movietype1, movietype2, movietype3, movietype4, movietype5,
+              director1, director2, movierole1, movierole2, movierole3,
+              movierole4, average_star, comment_num, content_tag1,
+              content_tag2, content_tag3, content_tag4, content_tag5,
+              content_tag6, content_tag7, content_tag8)
     # 执行sql语句
     cursor.execute(query, values)
 
--- Douban/spider.py
+++ Douban/spider.py
@@ -1,84 +1,115 @@
+# encoding=utf-8
+
 import requests
 from bs4 import BeautifulSoup
-import xlwt
+import pandas as pd
 import time
+import re
+#设置请求头
+headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 Edg/86.0.622.69622.69'
+}
 
 
-class Douban:
-    def __init__(self):
-        self.URL = 'https://movie.douban.com/top250'
-        self.startnum = []
-        for start_num in range(0, 251, 25):
-            self.startnum.append(start_num)
-        self.header = {
-            'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 Edg/86.0.622.69'
-        }
+#主函数入口
+def main():
+    base = "https://movie.douban.com/top250"
+    info = []
+    global rank
+    rank = 0
+    for page in range(0, 250, 25):
+        url = base + "?start=" + str(page)
+        html = get_html_text(url)
+        get_page_info(html, info)
+    data = pd.DataFrame(info)
 
-    def get_top250(self):
-        datalist = []
-        for start in self.startnum:
-            start = str(start)
-            html = requests.get(self.URL,
-                                params={'start': start},
-                                headers=self.header)
-            soup = BeautifulSoup(html.text, 'html.parser')
-            # 1.所有电影信息存储在ol标签
-            Obj = soup.find('div', class_='article')
-
-            if Obj != None:
-                details = Obj.find_all('li')
-                for detail in details:
-                    data = []
-                    #排名
-                    data.append(detail.find('em').get_text())
-                    # 电影名称
-                    data.append(detail.find('span', class_='title').get_text())
-                    #图片链接
-                    picture = detail.find('img')
-                    data.append(picture.get('src'))
-                    # 电影年份
-                    #抓取年份、国家
-                    actor_infos = detail.find('p').get_text().strip().split(
-                        '\n')
-                    actor_infos1 = actor_infos[0].split('\xa0\xa0\xa0')
-                    director = actor_infos1[0]
-                    data.append(director)
-                    if len(actor_infos1) >= 2:
-                        role = actor_infos1[1]
-                    else:
-                        role = ''
-                    data.append(role)
-                    year_area = actor_infos[1].lstrip().split('\xa0/\xa0')
-                    data.append(year_area[0])
-                    data.append(year_area[1])
-                    data.append(year_area[2])
-
-                    data.append(0)
-                    data.append(0)
-                    data.append('')
-                    datalist.append(data)
-                    print("爬取完", data[0])
-                time.sleep(1)
-
-        self.saveData(datalist)
-
-    def saveData(self, datalist):
-        workbook = xlwt.Workbook(encoding='utf-8')
-        worksheet = workbook.add_sheet('Doubantop250', cell_overwrite_ok=True)
-        col = ('rank', 'name', 'picturelink', 'director', 'role', ' year',
-               'country', 'movietype', 'average_star', 'comment_num',
-               'content_tag')
-        for i in range(0, 11):
-            worksheet.write(0, i, col[i])
-        for i in range(250):
-            data = datalist[i]
-            for j in range(0, 11):
-                worksheet.write(i + 1, j, data[j])
-                print("写入", i + 1, "条")
-        workbook.save('douban.xls')
+    data.to_excel('movies.xlsx', encoding="utf_8_sig")
 
 
-if __name__ == "__main__":
-    cls = Douban()
-    cls.get_top250()
+#提交请求，获取页面
+def get_html_text(url):
+    try:
+        r = requests.get(url, headers=headers)
+        r.raise_for_status()
+        return r.text
+    except requests.HTTPError:
+        return '爬取失败'
+
+
+#每一页
+def get_page_info(html, info):
+    soup = BeautifulSoup(html, 'html.parser')
+    movies = soup.find_all('div', attrs={'class':
+                                         'hd'})  #查找页面上所有div标签中，class=hd的tag对象。
+    for movie in movies:
+        get_movie_info(movie, info)
+
+
+#每一部电影
+def get_movie_info(movie, info):
+    a = movie.a
+    href = a.get('href')
+    details = {}
+    get_movie_details(href, details)
+    info.append(details)
+    time.sleep(1)
+
+
+#每一部电影的详细信息
+def get_movie_details(href, details):
+    global rank
+    rank = rank + 1
+
+    html = get_html_text(href)
+    soup = BeautifulSoup(html, 'html.parser')
+    moviename = soup.find('span', property="v:itemreviewed").text.split(' ')
+    details['排名'] = rank
+    details['电影名字'] = moviename[0]
+    picturelink = soup.find('img')
+    picturelink = picturelink.get('src')
+    details['图片链接'] = picturelink
+    attrs = soup.find_all('span', attrs={'class': 'pl'})
+    for attr in attrs:
+        if attr.string == '导演':
+            director = attr.next_sibling.next_sibling.text.split('/')
+            details['主导演'] = director[0]
+            if len(director) >= 2:
+                details['副导演'] = director[1]
+        if attr.string == '主演':
+            cast = attr.next_sibling.next_sibling.text.split('/')
+            i = 0
+            for cast1 in cast:
+                if (i >= 4):
+                    break
+                details['主演' + str(i + 1) + '号'] = cast1
+                i = i + 1
+        if attr.string == '类型:':
+            i = 1
+            genres = soup.find_all('span', attrs={'property': 'v:genre'})
+            for genre in genres:
+                details[('类型' + str(i))] = genre.string
+                i = i + 1
+        if attr.string == '制片国家/地区:':
+            details['国家/地区'] = attr.next_sibling
+        if attr.string == '上映日期:':
+            dates = soup.find_all('span',
+                                  attrs={'property': 'v:initialReleaseDate'})
+            date = ' / '.join([item.string for item in dates])
+            details['上映年份'] = date[0:4]
+        if attr.string == '片长:':
+            details['片长'] = attr.next_sibling.next_sibling.string
+    tags = soup.find('div', class_='tags-body').find_all(
+        'a', class_="")  #先找到tags-body的标签，然后找到a的子标签。
+    i = 1
+    for tag in tags:
+        details[('标签' + str(i))] = tag.text
+        i = i + 1
+    details['评价人数'] = 0
+    details['平均星数'] = 0
+    print(details)
+    print("已爬取完第" + str(rank) + "部")
+    time.sleep(1)
+
+
+main()
